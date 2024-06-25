c211587e83c056acca0bb0588436595a236aefc2
Rahul Tyagi <rahul.1992.tyagi@gmail.com>
2024-06-25 21:40:22 +0530

    feat: adds go package for Indian Pincodes

---
+++ pincodes-in-go/pincodes.go
@@ -0,0 +1,75 @@
+package pincodes
+
+import (
+	"embed"
+	"encoding/csv"
+	"strconv"
+)
+
+//go:embed pincode.csv
+var f embed.FS
+
+// "CircleName","RegionName","DivisionName","OfficeName","Pincode","OfficeType","Delivery",
+// "District","StateName","Latitude","Longitude"
+type Pincode struct {
+	CircleName   string
+	RegionName   string
+	DivisionName string
+	OfficeName   string
+	Pincode      int
+	OfficeType   string
+	Delivery     string
+	District     string
+	StateName    string
+	Latitude     float64
+	Longitude    float64
+}
+
+func Load() (map[int]Pincode, error) {
+	data, err := f.Open("pincode.csv")
+	if err != nil {
+		return nil, err
+	}
+
+	reader := csv.NewReader(data)
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	res := map[int]Pincode{}
+
+	for i, row := range records {
+		if i == 0 {
+			continue
+		}
+		record, err := rowToRecord(row)
+		if err != nil {
+			return nil, err
+		}
+
+		res[int(record.Pincode)] = *record
+	}
+
+	return res, nil
+}
+
+func rowToRecord(row []string) (*Pincode, error) {
+	pin, err := strconv.Atoi(row[4])
+	if err != nil {
+		return nil, err
+	}
+
+	return &Pincode{
+		CircleName:   row[0],
+		RegionName:   row[1],
+		DivisionName: row[2],
+		OfficeName:   row[3],
+		Pincode:      pin,
+		OfficeType:   row[5],
+		Delivery:     row[6],
+		District:     row[7],
+		StateName:    row[8],
+	}, nil
+}
---
+++ pincodes-in-go/pincodes_test.go
@@ -0,0 +1,20 @@
+package pincodes_test
+
+import (
+	"testing"
+
+	"github.com/dropdevrahul/pincodes-india/pincodes-in-go"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoad(t *testing.T) {
+	result, err := pincodes.Load()
+
+	assert.NoError(t, err)
+	assert.Equal(t, len(result), 19300)
+	assert.Equal(t, result[110001], pincodes.Pincode{
+		CircleName: "Delhi Circle", RegionName: "DivReportingCircle", DivisionName: "New Delhi Central Division",
+		OfficeName: "Parliament House SO", Pincode: 110001, OfficeType: "PO", Delivery: "Non Delivery", District: "NEW DELHI",
+		StateName: "DELHI", Latitude: 0, Longitude: 0,
+	})
+}
